5b08870f06e65172f506fa1573845f96e274c1b2
adamnapieralski <adam.napieralski@gmail.com>
2020-12-29 17:39:24 +0100

    remove client, webapp

--- webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('location-search', views.location_search, name='location_search'),
-    path('api/object-params', views.get_object_params, name='get_object_params')
-]
